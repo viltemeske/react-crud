2252800f15d396d9a984edd0cd54461068cfc988
viltemeske <meshkenai@gmail.com>
2023-02-20 17:01:13 +0200

    added ApiService to HomePage

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Container, Box } from '@mui/material';
+import ApiService from 'services/api-service';
 
-const HomePage = () => (
-  <Container sx={{ my: 6 }} />
-);
+const HomePage = () => {
+  const [animals, setAnimals] = React.useState<AnimalsModel[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      const fetchAnimals = await ApiService.fetchAnimals();
+      setAnimals(fetchAnimals);
+    })();
+  }, []);
+
+  return (
+    <Container sx={{ my: 6 }}>
+      <Box component="pre">{JSON.stringify(animals, null, 4)}</Box>
+    </Container>
+  );
+};
 
 export default HomePage;
