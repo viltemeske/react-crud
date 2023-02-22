b58d099910a157721afce5004ce13106fb124542
viltemeske <meshkenai@gmail.com>
2023-02-22 15:07:23 +0200

    created images field UX

--- client/src/pages/animal-form-page/images-field.tsx
+++ client/src/pages/animal-form-page/images-field.tsx
@@ -7,53 +7,52 @@ import {
   InputAdornment,
   IconButton,
 } from '@mui/material';
+import createId from 'uniqid';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
 
-const ImagesField = () => (
-  <Box sx={{ width: 1 }}>
-    <Typography component="legend">
-      <CameraAltOutlinedIcon />
-    </Typography>
-    <Stack sx={{ gap: 2 }}>
-      <TextField
-        sx={{ border: '1px solid black' }}
-        label="Nuotrauka"
-        fullWidth
-        variant="filled"
-        size="small"
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton>
-                <CloseOutlinedIcon color="error" />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-      />
-      <TextField
-        sx={{ border: '1px solid black' }}
-        label="Nuotrauka"
-        fullWidth
-        variant="filled"
-        size="small"
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton>
-                <CloseOutlinedIcon color="error" />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-      />
-    </Stack>
-    <IconButton>
-      <AddCircleIcon sx={{ fontSize: 38, color: 'primary.main' }} />
-    </IconButton>
-  </Box>
-);
+const initialIds = [createId()];
 
+const ImagesField = () => {
+  const [imgFieldsIds, setImgFieldsIds] = React.useState<string[]>(initialIds);
+
+  const addImgField = () => setImgFieldsIds([...imgFieldsIds, createId()]);
+  const removeImgField = (id: string) => {
+    if (imgFieldsIds.length > 1) {
+      setImgFieldsIds(imgFieldsIds.filter((imgId) => imgId !== id));
+    }
+  };
+
+  return (
+    <Box sx={{ width: 1 }}>
+      <Typography component="legend">
+        <CameraAltOutlinedIcon />
+      </Typography>
+      <Stack sx={{ gap: 2 }}>
+        {imgFieldsIds.map((id) => (
+          <TextField
+            sx={{ border: '1px solid black' }}
+            label="Nuotrauka"
+            fullWidth
+            variant="filled"
+            size="small"
+            InputProps={imgFieldsIds.length > 1 ? {
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={() => removeImgField(id)}>
+                    <CloseOutlinedIcon color="error" />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            } : undefined}
+          />
+        ))}
+      </Stack>
+      <IconButton onClick={addImgField}>
+        <AddCircleIcon sx={{ fontSize: 38, color: 'primary.main' }} />
+      </IconButton>
+    </Box>
+  );
+};
 export default ImagesField;
