1630a8868131ad281a5432b7dc131aa2696db0c1
viltemeske <meshkenai@gmail.com>
2023-02-22 14:37:14 +0200

    downloaded uniqid

--- client/src/pages/animal-form-page/styled.ts
+++ client/src/pages/animal-form-page/styled.ts
@@ -8,12 +8,12 @@ export const PageLayout = styled(Stack)(({ theme }) => ({
   alignItems: 'center',
   padding: theme.spacing(2),
   [theme.breakpoints.up('sm')]: {
-    paddingLeft: theme.spacing(6),
-    paddingRight: theme.spacing(6),
+    paddingTop: theme.spacing(6),
+    paddingBottom: theme.spacing(6),
   },
   [theme.breakpoints.up('xl')]: {
-    paddingLeft: theme.spacing(10),
-    paddingRight: theme.spacing(10),
+    paddingTop: theme.spacing(10),
+    paddingBottom: theme.spacing(10),
   },
 }));
 
